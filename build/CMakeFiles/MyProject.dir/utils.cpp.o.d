fa7a375ed5c65d9e6e283c49fa58065fed535114
Мария Рюпина <maryryryp@MacBook-Air-Maria-4.local>
2025-02-16 15:29:16 +0300

    Initial commit: CMake project setup

---
+++ build/CMakeFiles/MyProject.dir/utils.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/MyProject.dir/utils.cpp.o: \
+  /Users/maryryryp/my_project/src/utils.cpp
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,9 @@
+#include "utils.h"
+#include <iostream>
 
+int main() {
+    int a = 8, b = 5;
+    std::cout << "Сумма: " << add(a, b) << std::endl;
+    std::cout << "Разности: " << subtract(a, b) << std::endl;
+    return 0;
+}
